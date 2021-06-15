bef7c3243690ada931f37df5d89241fb6fb143b5
SJLEE316 <lsj8249@naver.com>
2021-06-15 21:01:48 +0900

    question_list.html -> basic setting

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from .views import * # home>views에서 모든 함수를 가져온다.
 app_name = "home"
 urlpatterns = [
   path('go_hello', hello, name="hello"), # home>views.py의 hello함수를 의미한다. 이름을 붙여준다.
+  path('question_list', question_list, name="question_list"), # 경로, 함수, 경로 이름
 ]
--- home/views.py
+++ home/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def hello(request):
   return render(request, 'home/hello.html')
+
+def question_list(request):
+  return render(request, 'home/question_list.html')
